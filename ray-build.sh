a0c8c22f650051ebe661645d614b73530fd6f024
Maxime Déraspe <maximilien1er@gmail.com>
2015-05-11 10:29:07 -0400

    edit ray-build.sh no need to compile RayPlatform twice..
    Signed-off-by: Maxime Déraspe <maximilien1er@gmail.com>

--- ray-build.sh
+++ ray-build.sh
@@ -3,16 +3,17 @@
 
 cd ray-build/
 
-# RayPlatform
-cd RayPlatform/
-make clean
-git pull
-make -j 2
-cd ../
+# # RayPlatform
+# cd RayPlatform/
+# make clean
+# git pull
+# make -j 2
+# cd ../
 
 # Ray
 cd ray/
 make clean
+rm -fr ./BUILD
 git pull
 make -j 2 PREFIX=`pwd`/BUILD MAXKMERLENGTH=64 HAVE_LIBZ=y HAVE_LIBBZ2=y ASSERT=n
 make install

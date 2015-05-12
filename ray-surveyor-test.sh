cbfca79bef34920d0d1c4ba6752127ebb64f2f99
Maxime Déraspe <maximilien1er@gmail.com>
2015-05-12 14:12:38 -0400

    add params in ray-surveyor-test.sh
    Signed-off-by: Maxime Déraspe <maximilien1er@gmail.com>

--- ray-surveyor-test.sh
+++ ray-surveyor-test.sh
@@ -3,7 +3,7 @@
 if [ -z "$1" ]
 then
     echo "Need a config file !"
-    echo "$ ray-surveyor-test.sh <config> <output to compare with> [Threads]"
+    echo "$ ray-surveyor-test.sh <config> <output to compare with> [Threads] [--diff]"
     exit 0
 fi
 
@@ -11,16 +11,26 @@ threads=2
 if [ ! -z "$2" ]
 then
     threads=$2
-    exit 0
 fi
 
-out=$(basename ${1%.conf})
+test_diff=0
+if [[ ! -z "$3" && $3 == "--diff" ]]
+then
+    test_diff=1
+fi
 
 mpiexec -n $threads ./Ray $1
 
-echo "Let's compare the output for $out"
+if [ $test_diff -eq 1 ]
+then
+    out=$(basename ${1%.conf})
+    
+    echo -e "\n\n"
+    echo "Let's compare the output for $out"
 
-# Difference between matrix
-diff surveys-fix/$out/Surveyor/SimilarityMatrix.tsv surveys-results/$out/Surveyor/SimilarityMatrix.tsv
-diff surveys-fix/$out/Surveyor/DistanceMatrix.tsv surveys-results/$out/Surveyor/DistanceMatrix.tsv
+    # Difference between matrix
+    diff surveys-fix/$out/Surveyor/SimilarityMatrix.tsv surveys-results/$out/Surveyor/SimilarityMatrix.tsv
+    diff surveys-fix/$out/Surveyor/DistanceMatrix.tsv surveys-results/$out/Surveyor/DistanceMatrix.tsv
+    
+fi
 
